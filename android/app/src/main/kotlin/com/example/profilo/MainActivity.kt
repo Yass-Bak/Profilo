8485a2beda1813ceeaa63080d3e7078e2589e1ea
Yass-Bak <baklouti.yessine@iit.ens.tn>
2024-04-27 23:05:53 +0100

    second commit

--- android/app/src/main/kotlin/com/example/profilo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/profilo/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.example.profilo
 
-import io.flutter.embedding.android.FlutterActivity
+//import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+//class MainActivity: FlutterActivity()
